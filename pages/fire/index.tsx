238eba1ab9f89330279136251ba5b973e8b385da
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-06-19 14:18:51 +0900

    add: Firestore読み込みページ

---
+++ pages/fire/index.tsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import Layout from '../../components/Layout';
+import firebase from 'firebase';
+import '../../components/fire';
+
+const db = firebase.firestore();
+
+export default function Home() {
+  const mydata: Object[] = [];
+  const [data, setData] = useState(mydata);
+  const [message, setMessage] = useState('wait...');
+
+  useEffect(() => {
+    db.collection('mydata')
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((document) => {
+          const doc = document.data();
+          mydata.push(
+            <tr key={document.id}>
+              <td>
+                <a href={'/fire/del?id=' + document.id}>{document.id}</a>
+              </td>
+              <td>{doc.name}</td>
+              <td>{doc.mail}</td>
+              <td>{doc.age}</td>
+            </tr>,
+          );
+        });
+        setData(mydata);
+        setMessage('Firebase data.');
+      });
+  }, []);
+
+  return (
+    <div>
+      <Layout header="Next.js" title="Top page.">
+        <div className="alert alert-primary text-center">
+          <h5 className="mb-4">{message}</h5>
+          <table className="table bg-white text-left">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>{data}</tbody>
+          </table>
+        </div>
+      </Layout>
+    </div>
+  );
+}
